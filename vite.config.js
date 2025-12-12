7d206b3756af35186a074418282aa921c5987f1c
Piyush Patel <manilalpatel2812@gmail.com>
2025-12-12 20:59:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Campus-Portal/',   // 👈 MUST MATCH YOUR REPOSITORY NAME
   plugins: [react()],
 })
